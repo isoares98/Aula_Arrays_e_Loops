7ca7fb0b71fd047c65a3d0c401e55b3e1588d022
Isa <isasoaresemilio80@gmai.com>
2023-08-17 23:40:55 -0300

    introdução array - explicação completa

--- 1_intro_arrays.js
+++ 1_intro_arrays.js
@@ -17,4 +17,28 @@ const nomesDeEstudantes = [`jose`, `felipe`, `mariana`];
 // Cada valor é associado a uma posição específica dentro do array, que é chamada de "índice".
 
 // index ou indice sempre começa do 0
-// então consequentemente 
+// então consequentemente jose é 0, felipe é 1 e mariana é 2
+// o tamanho desse array é 3
+
+console.log(nomesDeEstudantes); // quando coloco o comando console.log ele retornará todos os nomes dentro do array
+
+//para puxar um dado especifico dentro de um array temos que solicitar o index/indice desejado, como no exemplo abaixo
+//lembrando que você precisa acrescentar [] e dentro dele o numero do index/indece que deseja
+console.log(nomesDeEstudantes[0]);
+console.log(nomesDeEstudantes[1]);
+console.log(nomesDeEstudantes[2]);
+console.log(nomesDeEstudantes[3]); //aqui ele imprimi undefined >> pois não tem nada definido no campo desejado
+
+//Um array pode ser preechido igual uma variavel, utilizando number, strings e etc.
+// Exemplo:
+
+const notasEstudantes = [0, 1, 2, 3, 4, 5];
+// oberseve que temos do 0 a 5 de index/indeces, mas o tamanho do array é 6
+// dica: para descobrir o tamanho do array, é só somar +1 ao ultimo numero do index
+
+console.log(notasEstudantes[0]); //observe que aqui estou chamando pelo index/indece e não pela nota em si.
+console.log(notasEstudantes[1]);
+console.log(notasEstudantes[2]);
+console.log(notasEstudantes[3]);
+console.log(notasEstudantes[4]);
+console.log(notasEstudantes[5]);
