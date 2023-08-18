e453f16e169e02cee5b1e73f1b29dd56d68f85d2
Isa <isasoaresemilio80@gmai.com>
2023-08-17 21:57:38 -0300

    introdução da aula

---
+++ 1_intro_arrays.js
@@ -0,0 +1,20 @@
+//exemplo do que é uma array
+
+//variavel comuns
+
+const nome1 = "joao";
+const nome2 = "guilherme";
+const nome3 = "joaquim";
+
+//arrays são usadas quando tenho muitos dados para uma unica variavel, vejo o exemplo abaixo
+
+const nomesDeEstudantes = [`jose`, `felipe`, `mariana`];
+//Observe que para tornar uma variavel array usamos [ ]
+
+// o que é um array?
+// array em JavaScript pode ser comparado a uma coleção de itens, como uma lista de compras.
+// É uma maneira de armazenar vários valores (como números, strings, objetos ou outros tipos de dados) em uma única estrutura de dados organizada.
+// Cada valor é associado a uma posição específica dentro do array, que é chamada de "índice".
+
+// index ou indice sempre começa do 0
+// então consequentemente 
